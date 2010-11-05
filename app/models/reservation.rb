88931695b5eb019dbec23e95f28a6c1fee96a6bd
josh <devnull@localhost>
2010-11-05 15:11:13 -0600

    validate reservation dates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,11 @@ class Reservation < ActiveRecord::Base
 
   validates_associated :trip, :company
   validates_numericality_of :price, :allow_nil => true
-  validate :arrival_precedes_departure
+
+  validate do |res|
+    res.arrival_precedes_departure
+    res.within_trip_dates
+  end
 
   def to_s
     return Reservation.human_name if new_record?
@@ -28,18 +32,33 @@ class Reservation < ActiveRecord::Base
   end
 
   def arrival_place_and_time
-    pickup_location.to_s + "\n" +
-    arrival.strftime('%l:%M %P')
+    [ pickup_location, arrival_time ].
+      select { |e| not e.nil? }.
+      map { |e| e.to_s }.
+      join "\n"
   end
 
   def departure_place_and_time
-    dropoff_location.to_s + "\n" +
-    departure.strftime('%l:%M %P')
+    [ dropoff_location, departure_time ].
+      select { |e| not e.nil? }.
+      map { |e| e.to_s }.
+      join "\n"
   end
 
 private
   def arrival_precedes_departure
     errors.add :departure, I18n.t(:arrival_precedes_departure) unless
-      self.departure > self.arrival
+      self.departure >= self.arrival
+  end
+
+  def within_trip_dates
+    errors.add :arrival, I18n.t(:arrival_before_trip) unless
+      self.arrival >= self.trip.arrival
+    errors.add :arrival, I18n.t(:arrival_after_trip) unless
+      self.arrival <= self.trip.departure
+    errors.add :arrival, I18n.t(:departure_before_trip) unless
+      self.departure >= self.trip.arrival
+    errors.add :arrival, I18n.t(:departure_after_trip) unless
+      self.departure <= self.trip.departure
   end
 end
