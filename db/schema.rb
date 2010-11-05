fc341f0f62278fceb8f51d429a6d0f9098d86db4
josh <devnull@localhost>
2010-11-05 13:30:40 -0600

    up to date db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101105011427) do
+ActiveRecord::Schema.define(:version => 20101105191700) do
 
   create_table "clients", :force => true do |t|
     t.string   "nationality"
@@ -20,30 +20,48 @@ ActiveRecord::Schema.define(:version => 20101105011427) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "contact_gender"
+    t.string   "contact_title"
   end
 
   create_table "companies", :force => true do |t|
     t.string   "name"
     t.text     "address"
     t.string   "country"
-    t.string   "contact_name"
-    t.string   "email"
-    t.string   "phone"
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "city"
-    t.string   "main_phone"
-    t.string   "mobile_phone"
     t.string   "website"
     t.string   "kind"
+    t.string   "contact_general_name"
+    t.string   "contact_general_phone"
+    t.string   "contact_general_mobile"
+    t.string   "contact_general_email"
+    t.string   "contact_admin_name"
+    t.string   "contact_admin_phone"
+    t.string   "contact_admin_mobile"
+    t.string   "contact_admin_email"
+    t.string   "contact_reservation_name"
+    t.string   "contact_reservation_phone"
+    t.string   "contact_reservation_mobile"
+    t.string   "contact_reservation_email"
+    t.string   "bank_provider"
+    t.string   "bank_name"
+    t.string   "bank_address"
+    t.string   "bank_aba"
+    t.string   "bank_swift"
+    t.string   "bank_beneficiary"
+    t.string   "bank_client_account"
+    t.string   "bank_govt_id"
+    t.string   "bank_govt_id_type"
   end
 
   create_table "reservations", :force => true do |t|
     t.integer  "trip_id"
     t.integer  "company_id"
-    t.datetime "arrival"
-    t.datetime "departure"
+    t.date     "arrival"
+    t.date     "departure"
     t.string   "dropoff_location"
     t.string   "pickup_location"
     t.text     "services"
@@ -53,6 +71,12 @@ ActiveRecord::Schema.define(:version => 20101105011427) do
     t.string   "reservation_id"
     t.float    "price"
     t.float    "net_price"
+    t.boolean  "confirmed"
+    t.string   "confirmation_no"
+    t.boolean  "paid"
+    t.date     "paid_date"
+    t.string   "arrival_time"
+    t.string   "departure_time"
   end
 
   create_table "trips", :force => true do |t|
@@ -66,6 +90,9 @@ ActiveRecord::Schema.define(:version => 20101105011427) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
+    t.integer  "payment_pct"
+    t.text     "payment_date"
   end
 
 end
