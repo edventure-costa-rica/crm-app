287c07e0bd673e8fbce18e82c9dfff58120b5cbf
josh <devnull@localhost>
2010-11-08 18:36:05 -0600

    statuses

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,18 +1,38 @@
 class Trip < ActiveRecord::Base
+  class << self
+    attr_reader :statuses
+  end
+
+  @statuses = [
+    'pending',
+    'confirmed',
+    'canceled'
+  ]
+
   belongs_to :client
   has_many :reservations, :dependent => :delete_all,
     :order => 'arrival ASC, departure ASC'
 
   before_save :generate_default_values
 
-  validates_numericality_of :total_people, :greater_than => 0, :only_integer => true
+  validates_numericality_of :total_people,
+    :greater_than => 0, :only_integer => true
+  
+  validates_numericality_of :num_children, :num_disabled,
+    :allow_nil => true
 
-  validates_numericality_of :num_children, :num_disabled, :allow_nil => true
+  validates_inclusion_of :status, :in => Trip.statuses
 
   def to_s; new_record? ? Trip.human_name : self.registration_id; end
 
   def nights
     (departure.to_date - arrival.to_date).to_i
+  end
+
+  @statuses.each do |s|
+    define_method "#{s}?" do
+      self.status == s
+    end
   end
 
 protected
