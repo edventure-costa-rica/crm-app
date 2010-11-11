7986adc1648b96250279635a532e9493e860c666
josh <devnull@localhost>
2010-11-10 18:45:04 -0600

    create the list of people automatically

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -10,10 +10,12 @@ class Trip < ActiveRecord::Base
   ]
 
   belongs_to :client
+  has_many :people, :order => 'created_at ASC'
   has_many :reservations, :dependent => :delete_all,
     :order => 'arrival ASC, departure ASC'
 
   before_save :generate_default_values
+  after_save :maintain_people_size
 
   validates_numericality_of :total_people,
     :greater_than => 0, :only_integer => true
@@ -54,4 +56,20 @@ protected
       self.registration_id = reg
     end
   end
+
+  def maintain_people_size
+    # too many
+    if people.size > total_people
+      extra = people.find :all, :order => 'created_at ASC',
+        :offset => total_people
+      extra.each { |p| p.destroy }
+    
+    # too few
+    else
+      people.create :country => client.nationality while
+        people.size < total_people
+    end
+    
+  end
+
 end
