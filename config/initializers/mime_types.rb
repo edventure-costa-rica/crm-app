56392ace62b19c23191c23469ea0f273ebe554dd
josh <devnull@localhost>
2010-11-14 15:56:30 -0600

    odt type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Mime::Type.register_alias "application/pdf", :pdf
+Mime::Type.register "application/vnd.oasis.opendocument.text", :odt
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
