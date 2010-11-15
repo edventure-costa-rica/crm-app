d9f6e7b2e38adda8f1088a8d25a740d6f0a0215a
josh <devnull@localhost>
2010-11-15 14:29:28 -0600

    confirmation needs the payment due date

--- app/reports/confirmation_report.rb
+++ app/reports/confirmation_report.rb
@@ -4,6 +4,7 @@ class ConfirmationReport < ProposalReport
   @template_filename = 'contrib/confirmation-template.odt'
 
   def initialize(trip)
+    @due_date = trip.arrival_date - 6.weeks
     super
   end
 end
