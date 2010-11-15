1d4b798d76532b0a911553071031191a0d60270c
josh <devnull@localhost>
2010-11-15 14:05:36 -0600

    itd help to specify the right filename

--- app/reports/confirmation_report.rb
+++ app/reports/confirmation_report.rb
@@ -1,7 +1,7 @@
 # (C) Copyright 2010 Josh Leder (slushie) <josh@ha.cr>
 
 class ConfirmationReport < ProposalReport
-  @template_filename = 'contrib/proposal-template.odt'
+  @template_filename = 'contrib/confirmation-template.odt'
 
   def initialize(trip)
     super
