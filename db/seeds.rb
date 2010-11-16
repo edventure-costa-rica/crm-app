a895fa8f783ca54b48e214d308da40b9fdf57fea
josh <devnull@localhost>
2010-11-15 21:12:28 -0600

    import the hotels spreadsheet

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,66 @@ end
 
 # import from the hotel CSV
 require 'csv'
-CSV.open 'contrib/hotels.csv', 'r' do |row|
-  # ...
+@fields = {
+#  :country => 1,
+  :package => 2,
+  :name => 4,
+  :city => 5,
+  :package_length => 6,
+  :contact_general_name => 9,
+  :contact_general_email => 10,
+  :website => 11,
+  :contact_general_phone => 12,
+  :contact_general_mobile => 13,
+  :bank_provider => 15,
+  :bank_name => 16,
+  :bank_address => 17,
+  :bank_aba => 18,
+  :bank_swift => 19,
+  :bank_account => 20,
+  :bank_beneficiary => 21,
+  :bank_client_account => 22,
+  :bank_govt_id => 23
+}
+
+# cache regions
+@regions = {}
+['Costa Rica', 'Panama', 'Nicaragua', 'Honduras', 'Guatemala'].each do |c|
+  id = c.gsub(/\s/, '').downcase
+  @regions[id] = Region.find_by_country c, :first
+  puts "Country '#{id}' using region #{@regions[id]}"
 end
+
+@row = 0
+CSV.open 'contrib/hotels.csv', 'r' do |row|
+  @row += 1
+
+  # two lines of headers... brilliant
+  next if @row < 3
+
+  # tons of blank lines to skip
+  next if row[1].blank?
+  country = row[1].gsub(/\s/,'').downcase
+
+  unless @regions.has_key?(country)
+    puts "Skipping row #{@row} with invalid country '#{country}'"
+    next
+  end
+
+  attribs = { :kind => 'hotel', :region => @regions[country] }
+  @fields.each_pair { |field, column| attribs[field] = row[column] }
+
+  # i don't really understand why she lists hotels with no name
+  if attribs[:name] !~ /[A-Za-z]/
+    puts "Skipping unnamed hotel at row #{@row}"
+    next
+  end
+
+  begin
+    Company.create(attribs)
+  rescue => ex
+    puts "Failed to create hotel at row #{@row}: #{ex}"
+  end
+end
+
+puts "Created #{@rows} hotels"
