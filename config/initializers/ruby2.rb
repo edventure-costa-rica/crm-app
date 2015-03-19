e0923804a5c9fb8f27f3d74294ba8460eaa1ecc8
Josh Leder <josh@ha.cr>
2015-03-19 12:45:07 -0600

    monkey patch. i dont know what it does

---
+++ config/initializers/ruby2.rb
@@ -0,0 +1,22 @@
+# @see http://blog.lucascaton.com.br/index.php/2014/02/28/have-a-rails-2-app-you-can-run-it-on-the-newest-ruby/
+
+# This is a very important monkey patch to make Rails 2.3.18 to work with Ruby 2.x
+# If you're thinking to remove it, really, don't, unless you know what you're doing.
+# KTHXBYE
+
+if Rails::VERSION::MAJOR == 2 && RUBY_VERSION >= '2.0.0'
+  module ActiveRecord
+    module Associations
+      class AssociationProxy
+        def send(method, *args)
+          if proxy_respond_to?(method, true)
+            super
+          else
+            load_target
+            @target.send(method, *args)
+          end
+        end
+      end
+    end
+  end
+end
