c37975c75edea90104ac95a18d5db4118fcd0870
Josh Leder <josh@ha.cr>
2015-12-03 12:13:00 -0600

    refactor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,22 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function () {
-  $('#reservation_confirmed').on('change', function(ev, skipFocus) {
-    if ($(this).is(':checked')) {
-      var toggle = $('#toggle_confirm').show();
-      if (! skipFocus) toggle.find('input').focus();
+
+  function toggleWithCheckbox(target, source, focus) {
+    if ($(source).is(':checked')) {
+      var $target = $(target).show();
+      if (focus) $target.find('input').focus();
     }
     else {
-      $('#toggle_confirm').hide()
+      $(target).hide();
     }
+  }
+
+  $('#reservation_confirmed').on('change', function(ev, skipFocus) {
+    toggleWithCheckbox('#toggle_confirm', this, !skipFocus)
+  }).triggerHandler('change', true);
+
+  $('#reservation_paid').on('change', function(ev, skipFocus) {
+    toggleWithCheckbox('#toggle_paid', this, !skipFocus)
   }).triggerHandler('change', true);
 });
