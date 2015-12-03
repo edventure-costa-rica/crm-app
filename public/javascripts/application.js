39870575c1b4e8212da47484112135829d9f90d6
Josh Leder <josh@ha.cr>
2015-12-03 11:21:16 -0600

    toggle field on click

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function () {
+  $('#reservation_confirmed').on('change', function(ev, skipFocus) {
+    if ($(this).is(':checked')) {
+      var toggle = $('#toggle_confirm').show();
+      if (! skipFocus) toggle.find('input').focus();
+    }
+    else {
+      $('#toggle_confirm').hide()
+    }
+  }).triggerHandler('change', true);
+});
