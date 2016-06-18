c528723d1cf207ce00ace26614262cd669730529
Josh Leder <josh@ha.cr>
2016-06-18 11:54:41 -0600

    transport pick up /drop off lives in helper now

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -15,4 +15,35 @@ module ReservationsHelper
     }
   end
 
+  def transport_pick_up(res)
+    trip = res.trip
+
+    if not res.pick_up.to_s.empty?
+      res.pick_up
+
+    elsif trip.arrival == res.arrival
+      trip.arrival_flight
+
+    else
+      trip.reservations.
+          detect { |r| r.company.hotel? and r.departure == res.arrival }.
+          try(:company).try(:name)
+    end
+  end
+
+  def transport_drop_off(res)
+    trip = res.trip
+
+    if not res.drop_off.to_s.empty?
+      res.drop_off
+
+    elsif trip.departure == res.arrival
+      trip.departure_flight
+
+    else
+      trip.reservations.
+          detect { |r| r.company.hotel? and r.arrival == res.arrival }.
+          try(:company).try(:name)
+    end
+  end
 end
