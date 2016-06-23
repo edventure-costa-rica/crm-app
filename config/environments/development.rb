f001f253e7560ce695f24221dfb7f6e30965b1e6
Josh Leder <josh@ha.cr>
2016-06-22 20:03:09 -0600

    typo, fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,4 +15,4 @@ config.action_controller.perform_caching             = false
 
 
 config.action_mailer.raise_delivery_errors = true
-# config.action_mailer.perform_delivery = false
+config.action_mailer.perform_deliveries = false
