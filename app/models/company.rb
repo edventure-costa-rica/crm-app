6c1117bdf75c3d3878628ee3f69e5c6710fa8cbc
Josh Leder <josh@ha.cr>
2016-06-22 20:58:38 -0600

    encoding problems ;(

--- app/models/company.rb
+++ app/models/company.rb
@@ -48,8 +48,6 @@ class Company < ActiveRecord::Base
 
   validates_presence_of :bank_govt_id_type,
     :if => Proc.new { |co| not co.bank_govt_id.blank? }
-  validates_inclusion_of :bank_govt_id_type, :in => Company.govt_id_types,
-    :if => Proc.new { |co| not co.bank_govt_id_type.blank? }
 
   has_many :reservations
   belongs_to :region
