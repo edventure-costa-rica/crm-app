d51daaaa36ca0c1969702bd06c2798d6cd0b21db
Josh Leder <josh@ha.cr>
2016-10-23 09:48:07 -0600

    proper reservation day and nights

--- db/migrate/20160228010216_change_reservation_dates.rb
+++ db/migrate/20160228010216_change_reservation_dates.rb
@@ -1,8 +1,16 @@
 class ChangeReservationDates < ActiveRecord::Migration
+  class Reservation < ActiveRecord::Base
+  end
+
   def self.up
     change_table :reservations do |t|
       t.integer :day, default: 0, null: false
       t.integer :nights, default: 1, null: false
+    end
+
+    migrate_reservation_dates
+
+    change_table :reservations do |t|
       t.rename :pickup_location, :pick_up
       t.rename :dropoff_location, :drop_off
       t.remove :arrival, :arrival_time,
@@ -18,5 +26,18 @@ class ChangeReservationDates < ActiveRecord::Migration
       t.date :departure
       t.string :departure_time
     end
+  end
+
+  def self.migrate_reservation_dates
+    Trip.find_each do |trip|
+      trip.reservations.each do |res|
+        day = res.arrival.to_date - trip.arrival.to_date
+        nights = res.departure.to_date - res.departure.to_date
+        
+        res.update_attributes day: day, nights: nights
+      end
+    end
+
+    Reservation.reset_column_information
   end
 end
